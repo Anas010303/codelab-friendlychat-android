0f5fe5a672c7de55809955c85d880899d0d8b777
Anas010303 <105216729+Anas010303@users.noreply.github.com>
2024-03-16 13:40:42 +0200

    attempting authentication with firebase

--- build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.kt
+++ build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.kt
@@ -15,6 +15,7 @@
  */
 package com.google.firebase.codelab.friendlychat
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
@@ -25,8 +26,10 @@ import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.AuthUI.IdpConfig.*
 import com.google.firebase.BuildConfig
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.google.firebase.codelab.friendlychat.databinding.ActivityMainBinding
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.database
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.storage
@@ -40,6 +43,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     // TODO: implement Firebase instance variables
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var adapter: FriendlyMessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,9 +56,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // When running in debug mode, connect to the Firebase Emulator Suite.
-// "10.0.2.2" is a special IP address which allows the Android Emulator
-// to connect to "localhost" on the host computer. The port values (9xxx)
-// must match the values defined in the firebase.json file.
+        // "10.0.2.2" is a special IP address which allows the Android Emulator
+        // to connect to "localhost" on the host computer. The port values (9xxx)
+        // must match the values defined in the firebase.json file.
         if (BuildConfig.DEBUG) {
             Firebase.database.useEmulator("10.0.2.2", 9000)
             Firebase.auth.useEmulator("10.0.2.2", 9099)
@@ -61,6 +67,13 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Firebase Auth and check if the user is signed in
         // TODO: implement
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            // Not signed in, Launch the sign in activity
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+            return
+        }
 
         // Initialize Realtime Database and FirebaseRecyclerAdapter
         // TODO: implement
@@ -78,10 +91,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun getPhotoUrl(): String? {
+        val user = auth.currentUser
+        return user?.photoUrl?.toString()
+    }
+    private fun getUserName(): String? {
+        val user = auth.currentUser
+        return if (user != null){
+            user.displayName
+        }else ANONYMOUS
+    }
+
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in.
         // TODO: implement
+        if (auth.currentUser == null){
+            // Not signed in, launch the sign in activity
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+            return
+        }
     }
 
     public override fun onPause() {
@@ -119,6 +149,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun signOut() {
         // TODO: implement
+        AuthUI.getInstance().signOut(applicationContext)
+        startActivity(Intent (this, SignInActivity::class.java ))
+        finish()
     }
 
     companion object {
